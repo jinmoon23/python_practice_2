adea88b011431674dcb8d975ba33e9cb9a97e089
jinmun23 <wlsansdlek1@me.com>
2026-03-31 22:45:28 +0900

    practice: dijkstra day_3

---
+++ dijkstra/day_3.py
@@ -0,0 +1,41 @@
+import heapq
+
+def solutions(graph, start):
+  distances = {}
+  for node in graph:
+    distances[node] = float('inf')
+  
+  distances[start] = 0
+
+  paths = {start : [start]}
+
+  pq = []
+  heapq.heappush(pq, [start, distances[start]])
+
+  while (pq): 
+    curr_node, curr_dist = heapq.heappop(pq)
+    if distances[curr_node] < curr_dist: continue
+
+    neighbor_nodes = graph[curr_node]
+    for node in neighbor_nodes:
+      weight = neighbor_nodes[node]
+      distance = weight + curr_dist
+      if distance < distances[node]:
+        distances[node] = distance
+        heapq.heappush(pq, [node, distance])
+        new_path = paths[curr_node] + [node]
+        paths[node] = new_path
+  
+  return [distances, paths]
+print(solutions({
+  'A': {'B': 9, 'C': 3},
+  'B': {'A': 5}, 
+  'C': {'B': 1}
+  }, 'A'))
+
+print(solutions({
+  'A': {'B': 1},
+  'B': {'C': 5},
+  'C': {'D': 1},
+  'D': {}
+}, 'A'))
